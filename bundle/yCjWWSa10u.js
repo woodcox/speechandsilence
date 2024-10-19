f86ac731ab38efc7e2662afbd2dbb3f8ec883689
woodcox <woodcox@users.noreply.github.com>
2024-10-19 10:03:01 +0000

    deploy: 3c26d23405ab49c78a06013773eaa33187d09b99

---
+++ bundle/yCjWWSa10u.js
@@ -0,0 +1,1 @@
+(()=>{var c=document.querySelectorAll("button[data-index]"),o=document.querySelectorAll("dialog"),l=document.querySelectorAll("dialog button");c.forEach((t,e)=>{t.addEventListener("click",()=>{o[e].showModal()})});l.forEach((t,e)=>{t.addEventListener("click",()=>{o[e].close()})});window.addEventListener("click",t=>{o.forEach(e=>{t.target===e&&e.close()})});})();
